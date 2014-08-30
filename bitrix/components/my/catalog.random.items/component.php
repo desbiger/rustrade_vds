268c9e5da8c4b2a785573f4b804948636f78d362
Pavel Saharov <desbiger@gmail.com>
2014-08-30 11:44:02 +0400

    init

---
+++ bitrix/components/my/catalog.random.items/component.php
@@ -0,0 +1,34 @@
+<?
+	if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+		die();
+	}
+
+	$iblock_id = 2;
+	CModule::IncludeModule('iblock');
+	$q = "
+	SELECT
+	  el.*, prop.`VALUE`  AS PRICE
+	FROM
+	  b_iblock_element AS el
+	  LEFT JOIN `b_iblock_element_property` AS prop
+	    ON (
+	      el.`ID` = prop.`IBLOCK_ELEMENT_ID`
+	      AND prop.`IBLOCK_PROPERTY_ID` = 26
+	    )
+	WHERE el.`IBLOCK_ID` = {$iblock_id}
+	ORDER BY RAND()
+	LIMIT 10
+	";
+
+	$t = $DB->Query($q);
+	while ($temp = $t->Fetch()) {
+		$tovar = CIblockElement::GetByID($temp['ID'])->GetNext();
+		$temp['DETAIL_PAGE_URL'] = $tovar['DETAIL_PAGE_URL'];
+		$arResult['ITEMS'][] = $temp;
+	}
+
+
+	$this->IncludeComponentTemplate();
+
+
+?>
---
+++ bitrix/components/my/catalog.random.items/templates/.default/template.php
@@ -0,0 +1,30 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
+	die();
+} ?>
+<div class = "cat_tit">
+	<a href = "#">Популярные товары</a> <img src = "/bitrix/templates/Productions/img/marker2.png" alt = "m2">
+</div>
+<div class = "cat_tov">
+	<? foreach ($arResult['ITEMS'] as $item): ?>
+	<?
+		$this->AddEditAction($item['ID'], $item['EDIT_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_EDIT"));
+		$this->AddDeleteAction($item['ID'], $item['DELETE_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+	?>
+
+	<? $file = CFile::ResizeImageGet($item['DETAIL_PICTURE'], array(
+			'width' => 150,
+			'height' => 150
+	)); ?>
+
+	<div class = "tovar" id = "<?= $this->GetEditAreaId($item['ID']); ?>">
+		<a href = "<?= $item['DETAIL_PAGE_URL'] ?>">
+			<div style = "width:143px; height:130px; overflow:hidden;">
+				<img src = "<?= $file['src'] ?>" alt = "1">
+		</a>
+	</div>
+	<?= $item['NAME'] ?><br>
+	<br>
+<!--	<span class = "rr"> --><?//= $item['PRICE'] ?><!-- р.-</span>-->
+</div>
+<? endforeach ?>
+</div>
