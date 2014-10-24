d40767873926f23427ee47c9305476c00a9008d4
pavel <desbiger@gmail.com>
2014-10-24 12:00:08 +0400

    Вывод готовых проектов только для админа

--- bitrix/templates/Productions/footer.php
+++ bitrix/templates/Productions/footer.php
@@ -59,7 +59,7 @@
 </div>
 </div>
 <!-- #wrapper -->
-<?if($_SERVER['SCRIPT_NAME'] == '/index.php'):?>
+<?if($_SERVER['SCRIPT_NAME'] == '/index.php' && $USER->GetID() == 1):?>
 <div class = "ready_projects">
 
 
