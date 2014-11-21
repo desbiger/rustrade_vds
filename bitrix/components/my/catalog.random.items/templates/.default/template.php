91a0bc974bef8082404624a5ffaa1a02a4e8588d
pavel <desbiger@gmail.com>
2014-11-21 15:36:16 +0300

    Вывод типа цены на главной странице

--- bitrix/components/my/catalog.random.items/templates/.default/template.php
+++ bitrix/components/my/catalog.random.items/templates/.default/template.php
@@ -1,10 +1,12 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) {
 	die();
 } ?>
-<div class = "cat_tit">
-	<a href = "#">Популярные товары</a> <img src = "/bitrix/templates/Productions/img/marker2.png" alt = "m2">
+<!--<pre>--><? //print_r($arResult)?><!--</pre>-->
+
+<div class="cat_tit">
+	<a href="#">Популярные товары</a> <img src="/bitrix/templates/Productions/img/marker2.png" alt="m2">
 </div>
-<div class = "cat_tov" style = "width: 800px">
+<div class="cat_tov" style="width: 800px">
 	<? foreach ($arResult['ITEMS'] as $item): ?>
 	<?
 		$this->AddEditAction($item['ID'], $item['EDIT_LINK'], CIBlock::GetArrayByID($item["IBLOCK_ID"], "ELEMENT_EDIT"));
@@ -16,16 +18,16 @@
 			'height' => 150
 	)); ?>
 
-	<div class = "tovar" id = "<?= $this->GetEditAreaId($item['ID']); ?>">
-		<a href = "<?= $item['DETAIL_PAGE_URL'] ?>">
-			<div style = "width:143px; height:130px; overflow:hidden;">
-				<img src = "<?= $file['src'] ?>" alt = "1">
+	<div class="tovar" id="<?= $this->GetEditAreaId($item['ID']); ?>">
+		<a href="<?= $item['DETAIL_PAGE_URL'] ?>">
+			<div style="width:143px; height:130px; overflow:hidden;">
+				<img src="<?= $file['src'] ?>" alt="1">
 		</a>
 	</div>
 	<?= $item['NAME'] ?><br>
 	<br>
-	<input type = "button" name = "" id = "" value = "В корзину" tovar = "<?= $item['ID'] ?>"/>
-	<span class = "rr"> <?= $item['PRICE'] ?> р.-</span>
+	<input type="button" name="" id="" value="В корзину" tovar="<?= $item['ID'] ?>"/>
+	<span class="rr"> <?= $item['PRICE'] ?> <?= $znak = $item['PROPS']['PRICECURRENCY']['VALUE'] ? $znak : 'р.' ?>-</span>
 </div>
 <? endforeach ?>
 </div>
